bada3366674e4ea8fb413280435a67cb6a12c19c
anushka-here <anushkasingh8871@gmail.com>
2026-09-08 02:56:31 +0530

    new

--- week3/task4.java
+++ week3/task4.java
@@ -1,5 +1,88 @@
-package week2;
+package week3;
 
 public class task4 {
     
+    static class Employee {
+    String name;
+    int employeeId;
+    double salary;
+
+    Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    void displayEmployee() {
+        System.out.println("Name        : " + name);
+        System.out.println("Employee ID : " + employeeId);
+        System.out.println("Salary      : " + salary);
+    }
+}
+
+// Child class
+static class Developer extends Employee {
+
+    String programmingLanguage;
+
+    Developer(String name, int employeeId, double salary,
+              String programmingLanguage) {
+
+        super(name, employeeId, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    void displayDeveloper() {
+        displayEmployee();
+        System.out.println("Programming Language : "
+                + programmingLanguage);
+        System.out.println("----------------------------");
+    }
+}
+
+// Child class
+static class Manager extends Employee {
+
+    int teamSize;
+
+    Manager(String name, int employeeId, double salary,
+            int teamSize) {
+
+        super(name, employeeId, salary);
+        this.teamSize = teamSize;
+    }
+
+    void displayManager() {
+        displayEmployee();
+        System.out.println("Team Size : " + teamSize);
+        System.out.println("----------------------------");
+    }
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+
+        Developer d = new Developer(
+                "Anushka",
+                101,
+                60000,
+                "Java"
+        );
+
+        Manager m = new Manager(
+                "Rahul",
+                102,
+                80000,
+                10
+        );
+
+        System.out.println("===== EMPLOYEE DETAILS =====");
+
+        System.out.println("Developer:");
+        d.displayDeveloper();
+
+        System.out.println("Manager:");
+        m.displayManager();
+    }
+}
 }
--- week3/task5.java
+++ week3/task5.java
@@ -1,5 +1,74 @@
-package week2;
+package week3;
 
 public class task5 {
     
+    // Method Overloading
+    static class Calculator {
+
+        int calculate(int a, int b) {
+            return a + b;
+        }
+
+        double calculate(double a, double b) {
+            return a + b;
+        }
+
+        int calculate(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
+
+    // Parent class
+    static class Vehicle {
+
+        void start() {
+            System.out.println("Vehicle is starting");
+        }
+    }
+
+    // Child class
+    static class Car extends Vehicle {
+
+        @Override
+        void start() {
+            System.out.println("Car starts with a key");
+        }
+    }
+
+    // Child class
+    static class Bike extends Vehicle {
+
+        @Override
+        void start() {
+            System.out.println("Bike starts with a self-start button");
+        }
+    }
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+
+        System.out.println("===== METHOD OVERLOADING =====");
+
+        Calculator c = new Calculator();
+
+        System.out.println("Two integers: "
+                + c.calculate(10, 20));
+
+        System.out.println("Two doubles: "
+                + c.calculate(10.5, 20.5));
+
+        System.out.println("Three integers: "
+                + c.calculate(10, 20, 30));
+
+        System.out.println();
+
+        System.out.println("===== METHOD OVERRIDING =====");
+
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+
+        v1.start();
+        v2.start();
+    }
+}
 }
